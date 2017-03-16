2c6b01930205f4c73f5120f509e6fa7a1e51b992
Nikolaj Ivancic <nik@congral.com>
2017-03-16 11:34:19 -0400

    no comment

---
+++ src/nav-bar.ts
@@ -0,0 +1,72 @@
+import {bindable, inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {ThemeSelector} from './theme-selector/theme-selector';
+
+@inject(EventAggregator)
+export class NavBar {
+  @bindable router = null;
+  ea: EventAggregator;
+  development: boolean;
+  subscription: any;
+  dialog: any;
+  menu: any;
+  themeSelector: ThemeSelector;
+
+  constructor(ea) {
+    this.ea = ea;
+    this.development = location.hostname === 'localhost';
+    this.subscription = this.ea.subscribe('router:navigation:complete', () => this.routeChanged());
+  }
+
+  attached() {
+    this.dialog = kendo.jQuery(this.themeSelector).kendoWindow({
+      width: '800px',
+      visible: false,
+      title: 'Themes',
+      actions: [
+        'Pin',
+        'Minimize',
+        'Maximize',
+        'Close'
+      ]
+    }).data('kendoWindow');
+
+    kendo.jQuery(this.themeSelector).parent().addClass('theme-window');
+
+    kendo.jQuery(this.menu).kendoMenu({
+      select: (e) => {
+        kendo.jQuery(this.menu).find('.k-state-selected').removeClass('k-state-selected');
+        kendo.jQuery(e.item).addClass('k-state-selected');
+      }
+    });
+  }
+
+  routeChanged() {
+    kendo.jQuery(this.menu).find('.k-state-selected').removeClass('k-state-selected');
+
+    let route = this.router.currentInstruction;
+
+    // prefix hash to the fragment
+    let fragment = `#${route.fragment}`;
+
+    // check if any anchor's href is at the start of the fragment
+    kendo.jQuery('a', this.menu).each((i, anchor) => {
+      let href = kendo.jQuery(anchor).attr('href');
+
+      if (href !== '#' && fragment.startsWith(href)) {
+        kendo.jQuery(anchor).addClass('k-state-selected');
+      }
+    });
+  }
+
+  showThemeDialog() {
+    this.dialog.open();
+    this.dialog.center();
+  }
+
+  detached() {
+    if (this.subscription) {
+      this.subscription.dispose();
+    }
+  }
+}
