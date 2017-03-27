c6097322820948c8cc7983c40a128daf2a2c54c2
Daniel Bendel <daniel.bendel@gmail.com>
2017-03-27 07:33:25 +0200

    chore(all): fix menu layout, read router config

--- src/components/sample-runner.ts
+++ src/components/sample-runner.ts
@@ -1,6 +1,6 @@
 import {inject, bindable} from 'aurelia-framework';
 import {TemplatingEngine, ViewResources} from 'aurelia-templating';
-import {activationStrategy} from 'aurelia-router';
+import {activationStrategy, RouteConfig, Router} from 'aurelia-router';
 import {EventAggregator} from 'aurelia-event-aggregator';
 import {TaskQueue} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
@@ -8,6 +8,16 @@ import {Settings} from '../settings';
 
 @inject(EventAggregator, TaskQueue, Settings, TemplatingEngine, ViewResources)
 export class SampleRunner {
+  private category: any;
+  private router: Router;
+  private routes: any;
 
-
+  public activate(params: any, routeConfig: RouteConfig) {
+    this.category = routeConfig.category;
+    this.routes = routeConfig.navModel.router.routes;
+    let baseModuleId = routeConfig.baseModuleId;
+    this.router = routeConfig.navModel.router;
+    this.routes = this.router.routes.filter(r => r.category === this.category && r.baseModuleId === baseModuleId);
+    console.log('activate', arguments);
+  }
 }
