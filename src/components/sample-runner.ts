cf9d802e432a2ea6d46968c13bea180cdd9199e4
Daniel Bendel <daniel.bendel@gmail.com>
2017-03-31 07:39:18 +0200

    fix(sample-runner): fix sample navigation

--- src/components/sample-runner.ts
+++ src/components/sample-runner.ts
@@ -1,4 +1,5 @@
 import {inject, bindable} from 'aurelia-framework';
+import {getLogger, Logger} from 'aurelia-logging';
 import {TemplatingEngine, ViewResources} from 'aurelia-templating';
 import {activationStrategy, RouteConfig, Router} from 'aurelia-router';
 import {EventAggregator} from 'aurelia-event-aggregator';
@@ -6,11 +7,16 @@ import {TaskQueue} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-http-client';
 import {Settings} from '../settings';
 
-@inject(EventAggregator, TaskQueue, Settings, TemplatingEngine, ViewResources)
+// @inject(EventAggregator, TaskQueue, Settings, TemplatingEngine, ViewResources)
 export class SampleRunner {
   private category: any;
   private router: Router;
   private routes: any;
+  private log: Logger;
+
+  constructor() {
+    this.log = getLogger('sample-runner');
+  }
 
   public activate(params: any, routeConfig: RouteConfig) {
     this.category = routeConfig.category;
@@ -18,6 +24,9 @@ export class SampleRunner {
     let baseModuleId = routeConfig.baseModuleId;
     this.router = routeConfig.navModel.router;
     this.routes = this.router.routes.filter(r => r.category === this.category && r.baseModuleId === baseModuleId);
-    console.log('activate', arguments);
+  }
+
+  public determineActivationStrategy() {
+    return activationStrategy.replace;
   }
 }
